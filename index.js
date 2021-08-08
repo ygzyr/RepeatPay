b6e91d3519cf6508afcb9d3b87dafe32319c3d83
Mert Doğu <mertdoguonder@gmail.com>
2021-08-08 22:32:23 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const app = express()
 
 app.set('view engine', 'ejs');
 
-app.get('/:numara/:isim', function (req, res) {
+app.get('/:numara/:isim/:desc/:pfp/:img/', function (req, res) {
   if(req.params.numara.length>10 || req.params.numara.length<10){
       res.json({ "status":404, "desc":"gecersiz-num" })
   } else {
-      res.render('index', {numara: req.params.numara, isim: req.params.isim});
+      res.render('index', {numara: req.params.numara, isim: req.params.isim, desc: req.params.desc, pfp: req.params.pfp, img: req.params.img});
+
   }  
 })
+
 
 app.listen(80)
 console.log(`Your app is listening on http://localhost`)
