f2177c1deb34a649b5408817cbf865a5128093a7
Mert Doğu <mertdoguonder@gmail.com>
2021-08-11 10:59:34 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const app = express()
+const { base64encode, base64decode } = require('nodejs-base64');
 
 app.set('view engine', 'ejs');
 
 app.get('/:numara/:isim/:desc/:pfp/:img/', function (req, res) {
-  if(req.params.numara.length>10 || req.params.numara.length<10){
+  let numara = base64decode(req.params.numara);
+  let isim = base64decode(req.params.isim);
+  let desc = base64decode(req.params.desc);
+  let pfp = base64decode(req.params.pfp);
+  let img = base64decode(req.params.img);
+  if(numara.length>10 || numara.length<10){
       res.json({ "status":404, "desc":"gecersiz-num" })
   } else {
-      res.render('index', {numara: req.params.numara, isim: req.params.isim, desc: req.params.desc, pfp: req.params.pfp, img: req.params.img});
+      console.log(`${numara}/${isim}/${desc}/${pfp}/${img}`)
+      
+      res.render('index', {numara: numara, isim: isim, desc: desc, pfp: pfp, img: img});
 
   }  
 })
